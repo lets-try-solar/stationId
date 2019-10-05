d989069f276615ac870e1cfc7d6bcb5ed696ec1d
hemmi <hemmi@xmsoft.de>
2019-10-05 13:48:07 +0200

    first commit

---
+++ get_stationId.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+
+scriptpath="$( cd "$(dirname "$0")" ; pwd -P )"
+username="EMAIL-ADDRESS"
+password="PASSWORD"
+
+wget -qO- -o /dev/null --keep-session-cookies --save-cookies ${scriptpath}/cookies.txt --post-data "username=${username}&pwd=${password}" http://www.envertecportal.com/apiaccount/login > /dev/null 2>&1
+wget -q --load-cookies ${scriptpath}/cookies.txt 'http://www.envertecportal.com/terminal/systemoverview' -O ${scriptpath}/systemoverview > /dev/null 2>&1
+
+stationId=`grep "var stationId = '" ${scriptpath}/systemoverview | awk '{print $4}' | sed "s/'//g" | sed "s/;//g"`
+echo "StationId: ${stationId}"
+rm "${scriptpath}/systemoverview"
